b1d8d0f2c3f4163328b1b0cb7967f3af10bc9f94
Antti Juntunen <mfb0209@hotmail.fi>
2020-02-03 23:59:49 +0200

    kokeilu

--- models/person.js
+++ models/person.js
@@ -1,7 +1,38 @@
+/*
+const mongoose = require('mongoose');
+const url = process.env.MONGODB_URI;
+// Validaattori unique-arvoja varten
+const uniqueValidator = require('mongoose-unique-validator');
+
+// useFindAndModify-asetus falseksi, jotta findByIdAndUpdate()-metodi toimisi
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect(url, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}).then(success => 
+    console.log('Connection established', url))
+  .catch(error => console.log('Error with connection', error))
+
+schema = new mongoose.Schema({
+  name: {type: String, minlength: 3, unique: true},  
+  number: {type: String, minlength: 8}
+})
+
+schema.plugin(uniqueValidator)
+
+schema.set('toJSON', {
+  transform: (document, receivedObject) => {
+    receivedObject.id = receivedObject._id
+    delete receivedObject._id
+    delete receivedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', schema)
+*/
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
-// Validaattori unique-arvoja varten
-const uniqueValidator = require('mongoose-unique-validator')
 
 // useFindAndModify-asetus falseksi, jotta findByIdAndUpdate()-metodi toimisi
 mongoose.set('useFindAndModify', false)
@@ -14,11 +45,9 @@ mongoose.connect(url, {
   .catch(error => console.log('Error with connection', error))
 
 schema = new mongoose.Schema({
-  name: {type: String, minlength: 3, unique: true},  
-  number: {type: String, minlength: 8}
+  name: String, 
+  number: String,
 })
-
-schema.plugin(uniqueValidator)
 
 schema.set('toJSON', {
   transform: (document, receivedObject) => {
